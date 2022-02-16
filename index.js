7a37b90b48537c04a2c7e5d55941237490432a88
Riya <riyamadaan404@gmail.com>
2022-02-16 22:33:07 +0530

    enquiry page

--- index.js
+++ index.js
@@ -24,22 +24,95 @@ function backToTop() {
   document.documentElement.scrollTop = 0;
 }
 
-function displayEnquiry(){
+function displayEnquiry() {
   const enq = document.getElementById("enquirydiv");
-  if(enq.style.display === 'none'){
-  enq.style.display = 'block';
-  enq.style.zIndex = 9;
+  if (enq.style.display === 'none') {
+    enq.style.display = 'block';
+    enq.style.zIndex = 9;
   }
-  else{
+  else {
     enq.style.display = 'none';
     enq.style.zIndex = 0;
-  
+
   }
 }
 
-function enquiryClose(){
+function enquiryClose() {
   const enq = document.getElementById("enquirydiv");
   enq.style.display = 'none';
   enq.style.zIndex = 0;
+}
+
+// ---------------------------form validation-----------------------------//
+
+var subjectObject = {
+  "DIVISION PHARMA": {
+    "FERMINI 20 TABLET": [],
+    "CORFINE D3 TABLET ": [],
+    "CORFINE PLUS TABLET": [],
+    "CORFINE D3 60K Capsules*": [],
+    "UTICELL 100 mg SR TABLET": [],
+    "DEFCROSS 6 mg*": [],
+    "GASCROSS SUSPENSION (Mint Flavour)": [],
+    "GASCROSS - DSR CAPSULES": [],
+    "NITPURE 100 mg SR TABLET": [],
+    "CUFFCROSS - A SYRUP": [],
+    "CUFFCROSS - D SYRUP": [],
+
+  },
+  "DIVISION FEMINA": {
+    "FERMINI 20 TABLET": [],
+    "CORFINE D3 TABLET ": [],
+    "CORFINE PLUS TABLET": [],
+    "NITPURE 100 mg SR TABLET": [],
+    "LETCROSS 2.5 MG": [],
+    "LETCROSS 5 MG SR*": [],
+    "GASCROSS SUSPENSION (Mint Flavour)": [],
+    "GASCROSS - DSR CAPSULES": [],
+    "YAMUNA 21 TABLETS*": [],
+    "ARGICROSS  SACHET*": [],
+    "GESTCROSS  200 CAPSULES*": [],
+
+  },
+  "DIVISION VETCROSS": {
+    "CEFTIGROW 1 gm INJ (Lyophilized)": [],
+    "LACTOLIME GOLD 5 Litre": [],
+    "LACTOLIME GOLD 20 Litre": [],
+    "LACTOLIME GOLD 10 Litre*": [],
+    "LACTOLIME GEL": [],
+    "LEVOCROSS INJECTION": [],
+    "NIMUDOL INJECTION (with DMSO)": [],
+    "WORMCROSS": [],
+    "WORMCROSS-R": [],
+    "WORMCROSS-DS*": [],
+
+  },
+
+  "DIVISION PETCROSS": {
+    "LIMECAL PET": [],
+    "WORMCROSS PET ": [],
+    "CEPHACELL Tablet 300 mg": [],
+    "CEPHACELL Tablet 600 mg": [],
+    "PETSGLOW Shampoo": [],
+    "PETSLAC": [],
+
+  }
+}
+window.onload = function () {
+  var division = document.getElementById("divselect");
+  var medicine = document.getElementById("medselect");
+  for (var x in subjectObject) {
+    division.options[division.options.length] = new Option(x, x);
+  }
+  division.onchange = function () {
+    medicine.length = 1;
+    for (var y in subjectObject[this.value]) {
+      medicine.options[medicine.options.length] = new Option(y, y);
+    }
+  }
+  medicine.onchange = function () {
+    var z = subjectObject[division.value][this.value];
+  }
+
 }
 
